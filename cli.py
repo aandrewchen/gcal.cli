eddbb3b3ff68d3e79c2ebfb6e2f48d22d1cee392
aandrewchen <andche@ucdavis.edu>
2023-12-30 17:26:49 -0800

    Updated main code

--- cli.py
+++ cli.py
@@ -1,38 +1,13 @@
 import os
 
-from dotenv import load_dotenv
+from auth import create_calendar_service
 from get_event import get_upcoming_events
 from create_event import create_event
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
-load_dotenv()
 calendar_id = os.environ.get("CALENDAR_ID")
 
 def main():
-    creds = None
-
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-
-    with open("token.json", "w") as token:
-        token.write(creds.to_json())
-
-    service = build("calendar", "v3", credentials=creds)
+    service = create_calendar_service()
 
     print("Getting the upcoming 10 events")
     events = get_upcoming_events(service, calendar_id)
