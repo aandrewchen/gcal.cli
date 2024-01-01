866f82865b0930ed8eecbe6ef5146c2b8430ef5f
aandrewchen <andche@ucdavis.edu>
2024-01-01 12:55:14 -0800

    Added test for get command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,3 +13,8 @@ def test2_app():
     result = runner.invoke(app, ["create"])
     assert result.exit_code == 0
     assert "Creating an event" in result.stdout
+
+def test3_app():
+    result = runner.invoke(app, ["get"])
+    assert result.exit_code == 0
+    assert "2024-01-02T12:00:00-08:00 Weekly IET Meeting" in result.stdout
