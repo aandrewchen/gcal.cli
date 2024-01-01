6493a5bc1f5adde654243d69c000e664e33186c6
aandrewchen <andche@ucdavis.edu>
2024-01-01 12:52:09 -0800

    Made each command separate

--- cli.py
+++ cli.py
@@ -11,24 +11,27 @@ calendar_id = os.environ.get("CALENDAR_ID")
 
 app = typer.Typer()
 
-@app.command()
-def main(start: Annotated[str, typer.Argument()] = "get"):
-    service = get_auth()
+service = get_auth()
 
-    if start == "get":
-        print("Getting the upcoming 10 events")
-        events = get_upcoming_events(service, calendar_id)
-        if not events:
-            print("No upcoming events found.")
-        else:
-            for event in events:
-                start = event["start"].get("dateTime", event["start"].get("date"))
-                print(start, event["summary"])
-    elif start == "create":
-        print("Creating an event")
-        create_event(service, calendar_id)
-    elif start == "test":
-        print("Testing")
+@app.command()
+def create():
+    print("Creating an event")
+    create_event(service, calendar_id)
+
+@app.command()
+def get():
+    print("Getting the upcoming 10 events")
+    events = get_upcoming_events(service, calendar_id)
+    if not events:
+        print("No upcoming events found.")
+    else:
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
+
+@app.command()
+def test():
+    print("Testing")
 
 if __name__ == "__main__":
     app()
