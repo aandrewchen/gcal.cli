5848a78bd05f5a63373fe67408e884e1ab5b2938
aandrewchen <andche@ucdavis.edu>
2023-12-31 16:43:10 -0800

    Installed Typer library and starting to create CLI tool functionalities

--- cli.py
+++ cli.py
@@ -1,25 +1,31 @@
 import os
 
+import typer
+from typing import Optional
+from typing_extensions import Annotated
+
 from auth import get_auth
 from get_event import get_upcoming_events
 from create_event import create_event
 
 calendar_id = os.environ.get("CALENDAR_ID")
 
-def main():
+
+def main(start: Annotated[Optional[str], typer.Argument()] = None):
     service = get_auth()
 
-    print("Getting the upcoming 10 events")
-    events = get_upcoming_events(service, calendar_id)
-    if not events:
-        print("No upcoming events found.")
-    else:
-        for event in events:
-            start = event["start"].get("dateTime", event["start"].get("date"))
-            print(start, event["summary"])
-    
-    print("Creating an event")
-    create_event(service, calendar_id)
+    if start is None or start == "get":
+        print("Getting the upcoming 10 events")
+        events = get_upcoming_events(service, calendar_id)
+        if not events:
+            print("No upcoming events found.")
+        else:
+            for event in events:
+                start = event["start"].get("dateTime", event["start"].get("date"))
+                print(start, event["summary"])
+    elif start =="create":
+        print("Creating an event")
+        create_event(service, calendar_id)
 
 if __name__ == "__main__":
- main()
+    typer.run(main)
