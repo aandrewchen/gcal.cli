d577530af3780cf7a2a4fa146b8cc3ee3410fbed
aandrewchen <andche@ucdavis.edu>
2024-01-06 01:20:07 -0800

    Renamed unit tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -9,17 +9,17 @@ def test_app():
     assert result.exit_code == 0
     assert "Testing" in result.stdout
 
-def test_get():
+def test_get_command_no_number_specified():
     result = runner.invoke(app, ["get"])
     assert result.exit_code == 0
     assert "IET Work" in result.stdout
 
-def test_get_5():
+def test_get_command_5_specified():
     result = runner.invoke(app, ["get", "5"])
     assert result.exit_code == 0
     assert "IET Work" in result.stdout
 
-def test_create():
-    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "n", "--date", "2024-01-06", "--start", "08:00", "--end", "12:00", "--color", "Lavendar"])
+def test_create_command_with_arguments():
+    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "n", "--date", "2025-01-06", "--start", "08:00", "--end", "12:00", "--color", "Lavendar"])
     assert result.exit_code == 0
     assert "Creating an event" in result.stdout
