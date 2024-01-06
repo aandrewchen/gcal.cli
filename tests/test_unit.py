50e7071922752564e1ce21639e0fcbb59868da38
aandrewchen <andche@ucdavis.edu>
2024-01-06 02:25:13 -0800

    Added more unit tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -20,21 +20,30 @@ def test_get_command_5_specified():
     assert "IET Work" in result.stdout
 
 def test_create_command_with_arguments_not_recurring():
-    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "n", "--date", "2025-01-06", "--start", "08:00", "--end", "12:00", "--color", "Lavendar"])
+    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "n", "--date", "2025-01-06", "--start", "08:00", "--end", "12:00", "--color", "Lavendar", "--confirm", "y"])
     assert result.exit_code == 0
+    assert "Creating an event" in result.stdout
     assert "Event created" in result.stdout
 
 def test_create_command_with_arguments_recurring_one_day():
-    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "y", "--days", "Monday", "--enddate", "2025-01-06", "--date", "2024-01-08", "--start", "08:00", "--end", "12:00", "--color", "Banana"])
+    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "y", "--days", "Monday", "--enddate", "2025-01-06", "--date", "2024-01-08", "--start", "09:45", "--end", "16:10", "--color", "Banana", "--confirm", "y"])
     assert result.exit_code == 0
+    assert "Creating an event" in result.stdout
     assert "Event created" in result.stdout
 
 def test_create_command_with_arguments_recurring_multiple_days():
-    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "y", "--days", "Monday, Wednesday, Friday", "--enddate", "2025-01-06", "--date", "2024-01-08", "--start", "08:00", "--end", "12:00", "--color", "Basil"])
+    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "y", "--days", "Monday, Wednesday, Friday", "--enddate", "2025-01-06", "--date", "2024-01-08", "--start", "12:00", "--end", "14:00", "--color", "Basil", "--confirm", "y"])
     assert result.exit_code == 0
+    assert "Creating an event" in result.stdout
     assert "Event created" in result.stdout
 
 def test_create_command_with_arguments_recurring_multiple_days():
-    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "y", "--days", "Everyday", "--enddate", "2025-01-06", "--date", "2024-01-08", "--start", "08:00", "--end", "12:00", "--color", "Graphite"])
+    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "y", "--days", "Everyday", "--enddate", "2025-01-06", "--date", "2024-01-08", "--start", "08:00", "--end", "12:00", "--color", "Graphite", "--confirm", "y"])
     assert result.exit_code == 0
+    assert "Creating an event" in result.stdout
     assert "Event created" in result.stdout
+
+def test_create_command_with_arguments_recurring_days_no_confirm():
+    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "y", "--days", "Tuesday, Thursday", "--enddate", "2025-01-06", "--date", "2024-01-08", "--start", "07:15", "--end", "11:30", "--color", "Tomato", "--confirm", "n"])
+    assert result.exit_code == 0
+    assert "Event creation cancelled" in result.stdout
--- tests/test_unit_mock.py
+++ tests/test_unit_mock.py
@@ -32,3 +32,24 @@ def test_get_command_one_event(mock_get_upcoming_events):
 
     assert result.exit_code == 0
     assert "Test" in result.stdout
+
+@patch('cli.get_auth', new=mock_get_auth)
+@patch('cli.create_event')
+def test_create_command(mock_create_event):
+    mock_create_event.return_value = None
+
+    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "n", "--date", "2025-01-06", "--start", "08:00", "--end", "12:00", "--color", "Lavendar", "--confirm", "y"])
+
+    assert result.exit_code == 0
+    assert "Creating an event" in result.stdout
+    assert "Event created" in result.stdout
+
+@patch('cli.get_auth', new=mock_get_auth)
+@patch('cli.create_event')
+def test_create_command(mock_create_event):
+    mock_create_event.return_value = None
+
+    result = runner.invoke(app, ["create", "--summary", "Test", "--isrecurring", "n", "--date", "2025-01-06", "--start", "10:00", "--end", "15:00", "--color", "Peacock", "--confirm", "n"])
+
+    assert result.exit_code == 0
+    assert "Event creation cancelled" in result.stdout
