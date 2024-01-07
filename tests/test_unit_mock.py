a75a3e61eb742dca15cf2f648c16eb0ead29f59b
aandrewchen <andche@ucdavis.edu>
2024-01-06 22:54:59 -0800

    Added more unit tests

--- tests/test_unit_mock.py
+++ tests/test_unit_mock.py
@@ -1,6 +1,9 @@
 from unittest.mock import patch
 from cli import app
 from typer.testing import CliRunner
+import cli
+import unittest.mock
+import time
 
 runner = CliRunner()
 
@@ -53,3 +56,40 @@ def test_create_command(mock_create_event):
 
     assert result.exit_code == 0
     assert "Event creation cancelled" in result.stdout
+
+@patch('cli.get_auth', new=mock_get_auth)
+@patch('cli.get_upcoming_events')
+def test_listid_command(mock_get_upcoming_events):
+    mock_get_upcoming_events.return_value = []
+
+    result = runner.invoke(app, ["list-id"])
+
+    assert result.exit_code == 0
+    assert "Getting the next event's ID in your calendar" in result.stdout
+    assert "No upcoming events found." in result.stdout
+
+@patch('cli.get_auth', new=mock_get_auth)
+@patch('cli.get_upcoming_events')
+def test_listid_command_3_events(mock_get_upcoming_events):
+    mock_get_upcoming_events.return_value = [
+        {
+            'summary': 'Test',
+            'id': '1',
+        },
+        {
+            'summary': 'Test 2',
+            'id': '2',
+        },
+        {
+            'summary': 'Test 3',
+            'id': '3',
+        },
+    ]
+
+    result = runner.invoke(app, ["list-id", "3"])
+
+    assert result.exit_code == 0
+    assert "Getting the upcoming 3 events' ID's in your calendar" in result.stdout
+    assert "ID: 1 | Test" in result.stdout
+    assert "ID: 2 | Test 2" in result.stdout
+    assert "ID: 3 | Test 3" in result.stdout
